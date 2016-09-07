4450c4381d091b8e7a3e8b4ff17d70aef6e5abc8
Tommy Waters <Thomas.Waters05@gmail.com>
2016-09-06 21:26:57 -0500

    still working

---
+++ random.js
@@ -0,0 +1,8 @@
+document.getElementById("arrow").addEventListener("click", clickToRotate);
+
+randomnumber = function clickToRotate(){
+  Math.floor((Math.random()*100) + 1);
+  console.log("arrow")
+}
+
+
